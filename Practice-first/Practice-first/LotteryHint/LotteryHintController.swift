62127c18739e4bf180152ee080e0d451cbb3a789
Pathomphong Charoenwichianchay <beebbuild@gmail.com>
2019-08-11 10:40:13 +0700

    add lottery-hint project

---
+++ Practice-first/Practice-first/LotteryHint/LotteryHintController.swift
@@ -0,0 +1,9 @@
+//
+//  LotteryHint.swift
+//  Practice-first
+//
+//  Created by pathomphong charoenwichianchay on 10/8/2562 BE.
+//  Copyright © 2562 pathomphong charoenwichianchay. All rights reserved.
+//
+
+import Foundation
---
+++ Practice-first/Practice-first/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practice-first
+//
+//  Created by pathomphong charoenwichianchay on 10/8/2562 BE.
+//  Copyright © 2562 pathomphong charoenwichianchay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
